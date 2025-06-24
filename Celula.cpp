32b352d0746927dbf22b564b80e9854b7384614c
Clarice <claricefigueiredobarbos4@gmail.com>
2025-06-24 16:34:53 -0300

    template celula.cpp

--- Celula.cpp
+++ Celula.cpp
@@ -1,2 +1,23 @@
+#include "Celula.h"
+
+
+Celula::Celula() {}
+
+
+bool Celula::temBomba() {}
+
+bool Celula::taAberto() {}
+
+int Celula::qntVizinhos() {}
+
+
+bool Celula::colocaBomba() {}
+
+bool Celula::abrir() {}
+
+int Celula::colocaVizinhos(int qntV) {}
+
+
+Celula::~Celula() {}
 
 
