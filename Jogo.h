29dbe1b58459b8ae4109481405d5da68f33c70f3
GioSOliv <giovanna.souzaeoliveir@gmail.com>
2025-06-28 18:09:45 -0300

    commit do jogo.h

---
+++ Jogo.h
@@ -0,0 +1,18 @@
+#ifndef JOGO_H
+#define JOGO_H
+
+#include "Tabuleiro.h"
+#include <iostream>
+
+class Jogo
+{
+private:
+    Tabuleiro tabuleiro;
+public:
+    //Controla o jogo lendo as entradas dos jogadores
+    void jogo();
+    //Verifica se há ganhador, se quantidade >0 não ganhou, senão, ganhou
+    int Vencer();
+};
+
+#endif //JOGO_H
