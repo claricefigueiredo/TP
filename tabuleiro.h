c703facaa4e6a1af6475eb7e640321d7dd8d0c7b
andreigs-hub <gustavoandrei299@gmail.com>
2025-06-28 13:50:02 -0300

    Update tabuleiro.h

--- tabuleiro.h
+++ tabuleiro.h
@@ -3,3 +3,13 @@
 
 #include <vector>
 #include "Celula.h"  // Incluindo o cabeçalho da celula.h pois interage com tabuleiro
+
+class Tabuleiro {
+private:
+    std::vector<std::vector<Celula>> grade;
+    int tamanho;
+    int totalBombas;
+
+    bool coordenadaEhValida(int l, int c) const;
+    int contarBombasVizinhas(int l, int c) const;
+    void abrirCelulaRecursivo(int l, int c);
