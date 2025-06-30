67366e84e65b209847460dca85f4c5593c1eab23
ceciliafigueiredo <ceciliafigal@gmail.com>
2025-06-29 23:17:37 -0300

    Update Celula.cpp
    alteações bandeira

--- Celula.cpp
+++ Celula.cpp
@@ -1,9 +1,8 @@
+// Celula.cpp
 #include "Celula.h"
 
-
-Celula::Celula() : bomba(false), aberto(false), vizinhos(0) {
+Celula::Celula() : bomba(false), aberto(false), bandeira(false), vizinhos(0) {
 }
-
 
 bool Celula::temBomba() {
     return bomba;
@@ -13,10 +12,13 @@ bool Celula::taAberto() {
     return aberto;
 }
 
+bool Celula::temBandeira() {
+    return bandeira;
+}
+
 int Celula::qntVizinhos() {
     return vizinhos;
 }
-
 
 void Celula::colocaBomba() {
     bomba = true;
@@ -34,8 +36,5 @@ void Celula::colocaVizinhos(int qntV) {
     vizinhos = qntV;
 }
 
-
 Celula::~Celula() {
 }
-
-
